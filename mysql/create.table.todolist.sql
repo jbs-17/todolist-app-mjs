53764228b5fbcb19aca789355bae199659522817
jbs-17 <jatibangun17@gamil.com>
2025-11-11 22:06:20 +0700

    perbaikan konfigurasi dan struktur direktori 	renamed:    config/config.mjs -> configs/config.mjs 	renamed:    config/database.connection.config.mjs -> configs/database.connection.config.mjs 	renamed:    config/logger.config.mjs -> configs/logger.config.mjs 	deleted:    database/insert.user.mjs 	deleted:    database/update.user.password.mjs 	new file:   jsconfig.json 	modified:   mysql/create.table.todolist.sql 	modified:   package-lock.json 	modified:   package.json

--- mysql/create.table.todolist.sql
+++ mysql/create.table.todolist.sql
@@ -1,4 +1,4 @@
-CREATE TABLE todolist_simples (
+CREATE TABLE todolist_simple (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     user_id INT UNSIGNED NOT NULL,
     title VARCHAR(255) NOT NULL,
